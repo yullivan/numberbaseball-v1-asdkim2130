cea7684b927d0c5216cb855d0828901d5cdd0b24
HYOJIN <asdkim2130@gmail.com>
2024-11-25 17:13:58 +0900

    숫자 야구 게임 v1 - 스트라이크 개수 계산

--- src/main/java/numberbaseball/Application.java
+++ src/main/java/numberbaseball/Application.java
@@ -10,7 +10,14 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다!");
         System.out.println("1부터 9까지의 서로 다른 숫자 3개를 맞춰보세요.");
 
-        while (true) {
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        boolean isCorrect = true;
+        while (isCorrect) {
+
             // 사용자 입력 받기 (이 부분은 완성되어 있음)
             System.out.print("\n첫 번째 숫자를 입력하세요: ");
             int user1 = scanner.nextInt();
@@ -19,18 +26,26 @@ public class Application {
             System.out.print("세 번째 숫자를 입력하세요: ");
             int user3 = scanner.nextInt();
 
-            // TODO: strike 개수를 계산하세요
+                // TODO: strike 개수를 계산하세요
+            int strike = 0;
+                if (user1 == a) {
+                    strike = strike + 1;
+                }
+                if (user2 == b) {
+                    strike = strike + 1;
+                }
+                if (user3 == c){
+                    strike = strike + 1;}
+
+                // TODO: 결과를 출력하세요 (예: "1 스트라이크")
+                System.out.println(strike + " strike");
+
+                // TODO: 3 스트라이크인 경우 게임을 끝내세요
+
+                if(strike == 3){break;}
 
 
-            // TODO: 결과를 출력하세요 (예: "1 스트라이크")
-
-
-            // TODO: 3 스트라이크인 경우 게임을 끝내세요
-
-
-        }
-
-        // System.out.println("축하합니다! 정답을 맞추셨습니다.");
+        }System.out.println("축하합니다! 정답을 맞추셨습니다.");
     }
 
 }
